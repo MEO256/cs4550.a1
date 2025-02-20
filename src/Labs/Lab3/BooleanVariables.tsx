78550f33117ac39bcf804a49cf79df1764653d73
MEO256 <Leonidbeer@Gmail.com>
2025-02-19 22:39:31 -0500

    A3 Done

--- src/Labs/Lab3/BooleanVariables.tsx
+++ src/Labs/Lab3/BooleanVariables.tsx
@@ -1,4 +1,4 @@
-export default function VariablesAndConstants() {
+export default function BooleanVariables() {
 let numberVariable = 123, floatingPointNumber = 234.345;
 let true1 = true, false1 = false;
 let false2 = true1 && false1;
@@ -20,3 +20,4 @@ return (
     false3    = {false3 + ""}    <hr />
   </div>
 );}
+
